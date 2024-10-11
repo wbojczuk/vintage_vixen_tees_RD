dd83fa1e674064038a0d6bed2769b92fdf8e68e8
William Bojczuk <williambojczuk@gmail.com>
2024-10-11 15:55:04 -0400

    updated

--- src/app/shopify/shopContext.js
+++ src/app/shopify/shopContext.js
@@ -18,7 +18,7 @@ class ShopProvider extends Component {
     } else {
       this.createCheckout()
     }
-    if(!this.checkout.hasOwnProperty("lineItems")){
+    if(!("lineItems" in this.state.checkout)){
       createCheckout();
       console.log("created new chekout")
     }
